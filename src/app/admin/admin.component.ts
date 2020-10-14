0321c332d32d607a903e8463bfdee456862133eb
RostykStup <rostyk.stup@gmail.com>
2020-10-14 13:21:30 +0300

    pre-reinstall

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AdminComponent implements OnInit {
 
   tabs = [
-    {link: 'category', label: 'КАТЕГОРІЇ', clicked: true},
-    {link: 'subcategory', label: 'ПІДКАТЕГОРІЇ', clicked: false},
-    {link: 'moderators', label: 'МОДЕРАТОРИ', clicked: false}
+    {link: 'category', label: 'Категорії', clicked: true, block_end: false},
+    {link: 'subcategory', label: 'Підкатегорії', clicked: false, block_end: true},
+    {link: 'users', label: 'Користувачі', clicked: false, block_end: false},
+    {link: 'admins', label: 'Адміни', clicked: false, block_end: false},
+    {link: 'moderators', label: 'Модератори', clicked: false, block_end: false}
+
   ];
 
   constructor() { }
