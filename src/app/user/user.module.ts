deed434fef78cabf067e387f9b54db16104f2b32
RostykStup <rostyk.stup@gmail.com>
2020-10-21 22:57:15 +0300

    updated registration compinent

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import {UserComponent} from './user.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [UserComponent, RegistrationComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MatStepperModule
   ]
 })
 export class UserModule { }
